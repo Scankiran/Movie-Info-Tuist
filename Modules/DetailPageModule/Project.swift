4c2fb173910a9d699428c5f0c162ae1eb9becda1
Said ÇANKIRAN <cank32cank@gmail.com>
2022-09-21 17:01:01 +0300

    Project dependencies created. Base done.

--- Modules/DetailPageModule/Project.swift
+++ Modules/DetailPageModule/Project.swift
@@ -7,13 +7,22 @@
 
 import ProjectDescription
 
+let networkKitDependency = TargetDependency
+    .project(target: "NetworkKit", path: .relativeToRoot("Kits/NetworkKit"))
+
+let animationKitDependency = TargetDependency
+    .project(target: "AnimationKit", path: .relativeToRoot("Kits/AnimationKit"))
 
 let detailPageTarget = Target(name: "DetailPageModule",
-                            platform: .iOS,
-                            product: .staticFramework,
-                            bundleId: "com.saidcankiran.DetailPageModule",
-                            sources: "Sources/**",
-                            resources: "Resource/**"
+                              platform: .iOS,
+                              product: .staticFramework,
+                              bundleId: "com.saidcankiran.DetailPageModule",
+                              sources: "Sources/**",
+                              resources: "Resource/**",
+                              dependencies: [
+                                networkKitDependency,
+                                animationKitDependency
+                              ]
 )
 
 let project = Project(name: "DetailPageModule",
--- Modules/MainPageModule/Project.swift
+++ Modules/MainPageModule/Project.swift
@@ -10,6 +10,13 @@ import ProjectDescriptionHelpers
 
 let detailPageDependency: TargetDependency = .project(target: "DetailPageModule", path: .relativeToRoot("Modules/DetailPageModule"))
 
+
+let networkKitDependency = TargetDependency
+    .project(target: "NetworkKit", path: .relativeToRoot("Kits/NetworkKit"))
+
+let animationKitDependency = TargetDependency
+    .project(target: "AnimationKit", path: .relativeToRoot("Kits/AnimationKit"))
+
 let mainPageTarget = Target(name: "MainPageModule",
                             platform: .iOS,
                             product: .staticFramework,
@@ -17,7 +24,9 @@ let mainPageTarget = Target(name: "MainPageModule",
                             sources: "Sources/**",
                             resources: "Resource/**",
                             dependencies: [
-                                           detailPageDependency
+                                detailPageDependency,
+                                networkKitDependency,
+                                animationKitDependency
                             ])
 
 let project = Project(name: "MainPageModule",
